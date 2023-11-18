e9b525ad2fee26d5efe940b0768e601d9c087e2e
jgeun <pjk8926@gmail.com>
2023-11-18 15:48:54 +0900

    chore: compileSdk, targetSdk 33 -> 34 / minSdk 24 -> 26 변경

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,9 +1,9 @@
 import org.gradle.api.JavaVersion
 
 object Configuration {
-    const val compileSdk = 33
-    const val targetSdk = 33
-    const val minSdk = 24
+    const val compileSdk = 34
+    const val targetSdk = 34
+    const val minSdk = 26
     const val versionCode = 1
     const val versionName = "1.0"
     val javaVersion = JavaVersion.VERSION_1_8
