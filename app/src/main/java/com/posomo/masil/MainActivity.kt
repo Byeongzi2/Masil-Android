db289ef02fd6f0289bb4b827994b4bc0e85d0d4c
jgeun <pjk8926@gmail.com>
2023-11-18 15:49:45 +0900

    feat: Navigation 기능 임시 구현

--- app/src/main/java/com/posomo/masil/MainActivity.kt
+++ app/src/main/java/com/posomo/masil/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.posomo.masil
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.posomo.masil.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/posomo/masil/feature/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.posomo.masil.feature.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.posomo.masil.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+	private var _binding: FragmentHomeBinding? = null
+	private val binding get() = _binding!!
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = FragmentHomeBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+
+	companion object {
+		@JvmStatic
+		fun newInstance() = HomeFragment()
+	}
+}
